09ce3f5e56b7b0ce96640bac1377f70bc1fdee5b
Jakub <jakub@Jakubs-MacBook-Pro.local>
2020-03-18 18:22:52 +0100

    added login signup buttons to toolbar

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import {HomeComponent} from './home/home.component'
+import {TabBarNav} from './tabBar/tabbar.component'
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component:  HomeComponent},
+  { path: 'browse', component: TabBarNav },
+  { path: 'log_in', component: LoginComponent},
+  { path: 'sign_up', component: SignupComponent}
+];
+
+
+
+
+
+@NgModule({
+
+  imports: [RouterModule.forRoot(appRoutes)],
+ exports:[RouterModule]
+})
+export class AppRoutingModule { }
+export const routingComponents = [HomeComponent, TabBarNav, LoginComponent, SignupComponent]
---
+++ src/app/navbar/overflow-menu/overflow-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-overflow-menu',
+  templateUrl: './overflow-menu.component.html',
+  styleUrls: ['./overflow-menu.component.css']
+})
+export class OverflowMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
